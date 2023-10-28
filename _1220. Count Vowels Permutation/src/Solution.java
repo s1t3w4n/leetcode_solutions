5ef77e28cb126c3e6ed8444a8540233bf903749b
s1t3w4n <s1t3w4n@yandex.ru>
2023-10-28 20:46:11 +0300

    has added solution for 1220. Count Vowels Permutation

---
+++ _1220. Count Vowels Permutation/src/Solution.java
@@ -0,0 +1,59 @@
+/**
+ * Given an integer n, your task is to count how many strings of length n can be formed under the following rules:
+ * <p>
+ * Each character is a lower case vowel ('a', 'e', 'i', 'o', 'u')
+ * Each vowel 'a' may only be followed by an 'e'.
+ * Each vowel 'e' may only be followed by an 'a' or an 'i'.
+ * Each vowel 'i' may not be followed by another 'i'.
+ * Each vowel 'o' may only be followed by an 'i' or a 'u'.
+ * Each vowel 'u' may only be followed by an 'a'.
+ * Since the answer may be too large, return it modulo 10^9 + 7.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: n = 1
+ * Output: 5
+ * Explanation: All possible strings are: "a", "e", "i" , "o" and "u".
+ * Example 2:
+ * <p>
+ * Input: n = 2
+ * Output: 10
+ * Explanation: All possible strings are: "ae", "ea", "ei", "ia", "ie", "io", "iu", "oi", "ou" and "ua".
+ * Example 3:
+ * <p>
+ * Input: n = 5
+ * Output: 68
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= n <= 2 * 10^4
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().countVowelPermutation(1));
+        System.out.println(new Solution().countVowelPermutation(2));
+        System.out.println(new Solution().countVowelPermutation(5));
+    }
+
+    public int countVowelPermutation(int n) {
+        final int MOD = 1000000007;
+
+        long a = 1, e = 1, i = 1, o = 1, u = 1;
+
+        for (int j = 1; j < n; j++) {
+            long a_next = e;
+            long e_next = (a + i) % MOD;
+            long i_next = (a + e + o + u) % MOD;
+            long o_next = (i + u) % MOD;
+            long u_next = a;
+
+            a = a_next;
+            e = e_next;
+            i = i_next;
+            o = o_next;
+            u = u_next;
+        }
+
+        return (int)((a + e + i + o + u) % MOD);
+    }
+}
