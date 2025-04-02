c421b39e02e0a71e01d5323232208b3672fd7ea9
aleksey.a <s1t3w4n@yandex.ru>
2025-04-02 22:52:53 +0300

    have added solutions for 2873. Maximum Value of an Ordered Triplet I

---
+++ _2873. Maximum Value of an Ordered Triplet I/src/Solution2.java
@@ -0,0 +1,55 @@
+/**
+ * You are given a 0-indexed integer array nums.
+ * <p>
+ * Return the maximum value over all triplets of indices (i, j, k) such that i < j < k. If all such triplets have a negative value, return 0.
+ * <p>
+ * The value of a triplet of indices (i, j, k) is equal to (nums[i] - nums[j]) * nums[k].
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [12,6,1,2,7]
+ * Output: 77
+ * Explanation: The value of the triplet (0, 2, 4) is (nums[0] - nums[2]) * nums[4] = 77.
+ * It can be shown that there are no ordered triplets of indices with a value greater than 77.
+ * Example 2:
+ * <p>
+ * Input: nums = [1,10,3,4,19]
+ * Output: 133
+ * Explanation: The value of the triplet (1, 2, 4) is (nums[1] - nums[2]) * nums[4] = 133.
+ * It can be shown that there are no ordered triplets of indices with a value greater than 133.
+ * Example 3:
+ * <p>
+ * Input: nums = [1,2,3]
+ * Output: 0
+ * Explanation: The only ordered triplet of indices (0, 1, 2) has a negative value of (nums[0] - nums[1]) * nums[2] = -3. Hence, the answer would be 0.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 3 <= nums.length <= 100
+ * 1 <= nums[i] <= 10^6
+ */
+public class Solution2 {
+    public static void main(String[] args) {
+        // 77
+        System.out.println(new Solution2().maximumTripletValue(new int[]{12,6,1,2,7}));
+        // 133
+        System.out.println(new Solution2().maximumTripletValue(new int[]{1,10,3,4,19}));
+        // 0
+        System.out.println(new Solution2().maximumTripletValue(new int[]{1,2,3}));
+        // 999999000000
+        System.out.println(new Solution().maximumTripletValue(new int[]{1000000, 1, 1000000}));
+    }
+
+    public long maximumTripletValue(int[] nums) {
+        long result = 0;
+        int maxDifference = 0;
+        int max = 0;
+        for (int num : nums) {
+            result = Math.max(result, (long) maxDifference * num);
+            maxDifference = Math.max(maxDifference, max - num);
+            max = Math.max(max, num);
+        }
+        return result;
+    }
+}
