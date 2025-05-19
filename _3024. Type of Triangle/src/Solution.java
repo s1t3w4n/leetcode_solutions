137d5440982e4aed9c663e8e549dc244f5bb595f
aleksey.a <s1t3w4n@yandex.ru>
2025-05-19 23:14:40 +0300

    has added solutions for 3024. Type of Triangle

---
+++ _3024. Type of Triangle/src/Solution.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * You are given a 0-indexed integer array nums of size 3 which can form the sides of a triangle.
+ * <p>
+ * A triangle is called equilateral if it has all sides of equal length.
+ * A triangle is called isosceles if it has exactly two sides of equal length.
+ * A triangle is called scalene if all its sides are of different lengths.
+ * Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,3,3]
+ * Output: "equilateral"
+ * Explanation: Since all the sides are of equal length, therefore, it will form an equilateral triangle.
+ * Example 2:
+ * <p>
+ * Input: nums = [3,4,5]
+ * Output: "scalene"
+ * Explanation:
+ * nums[0] + nums[1] = 3 + 4 = 7, which is greater than nums[2] = 5.
+ * nums[0] + nums[2] = 3 + 5 = 8, which is greater than nums[1] = 4.
+ * nums[1] + nums[2] = 4 + 5 = 9, which is greater than nums[0] = 3.
+ * Since the sum of the two sides is greater than the third side for all three cases, therefore, it can form a triangle.
+ * As all the sides are of different lengths, it will form a scalene triangle.
+ * <p>
+ * Constraints:
+ * <p>
+ * nums.length == 3
+ * 1 <= nums[i] <= 100
+ */
+public class Solution {
+    public static void main(String[] args) {
+        // equilateral
+        System.out.println(new Solution().triangleType(new int[]{3, 3, 3}));
+        // scalene
+        System.out.println(new Solution().triangleType(new int[]{3, 4, 5}));
+        // none
+        System.out.println(new Solution().triangleType(new int[]{8, 4, 2}));
+        // none
+        System.out.println(new Solution().triangleType(new int[]{5, 3, 8}));
+    }
+
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+        if (nums[2] >= nums[1] + nums[0]) {
+            return "none";
+        }
+        Set<Integer> sizes = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        if (sizes.size() == 1) {
+            return "equilateral";
+        }
+        if (sizes.size() == 2) {
+            return "isosceles";
+        }
+        return "scalene";
+    }
+}
